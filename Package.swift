c65fba2a53f6c073d6dc9a28b0e8e0b444625285
Mikael Konradsson <mikael.konradsson@outlook.com>
2023-03-19 10:14:22 +0100

    Swift format changes

--- Package.swift
+++ Package.swift
@@ -3,25 +3,25 @@
 import PackageDescription
 
 let package = Package(
-  name: "loco",
-  platforms: [
-    .macOS(.v10_14)
-  ],
-  products: [
-    .executable(name: "loco", targets: ["loco"])
-  ],
-  dependencies: [
-    .package(url: "https://github.com/konrad1977/funswift", branch: "main"),
-  ],
-  targets: [
-    .executableTarget(
-      name: "loco",
-      dependencies: [
-        .product(name: "Funswift", package: "funswift")
-      ]),
-    .testTarget(
-      name: "LocoTests",
-      dependencies: ["loco"]
-    )
-  ]
+    name: "loco",
+    platforms: [
+        .macOS(.v10_14)
+    ],
+    products: [
+        .executable(name: "loco", targets: ["loco"])
+    ],
+    dependencies: [
+        .package(url: "https://github.com/konrad1977/funswift", branch: "main")
+    ],
+    targets: [
+        .executableTarget(
+            name: "loco",
+            dependencies: [
+                .product(name: "Funswift", package: "funswift")
+            ]),
+        .testTarget(
+            name: "LocoTests",
+            dependencies: ["loco"]
+        )
+    ]
 )
